e2d97ce12529bd36aad5a3c23c425410e97557fb
James Wyn <thisisjameswyn@gmail.com>
2021-04-09 11:03:48 -0700

    add initial logic and constructor

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+//choose pizza size (Sparrow,Magpie,Laridae,Xiongguanlong,)
+//choose sauce (red, white, bbq, salsa)
+//choose toppings (cheese, pepperoni, salami, hamburger, sausage, pinneapple, olives, bell pepper, sun dried tomatoes)
+//
+//
+//construct pizza, pass size value, build with toppings array
+//forEach (topping) add to array and factor out cost + + ++ ++ ++ +
+//return cost to user
+//cat party in the sky!!!!!!!!
+
+function Pizza(_toppings){
+  this.size = 0;
+  this.toppings = _toppings;
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+}
+
+
+$(document).ready(function () {
+  $("form#submit").submit(function (event) { 
+    event.preventDefault();
+    let toppings = $(".toppings").map(function() {
+      return this.value;
+    })
+
+    let myZa = new Pizza(toppings);
+
+    console.log(myZa);
+    
+  });
+});
